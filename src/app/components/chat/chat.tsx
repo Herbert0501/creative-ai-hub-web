040aa3229802981858018614999e95b3d5bf9135
K·Herbert <herbert501@qq.com>
2024-05-28 00:21:10 +0800

    add exit minsize maxsize button

--- src/app/components/chat/chat.tsx
+++ src/app/components/chat/chat.tsx
@@ -1,5 +1,9 @@
 import styles from "./chat.module.scss";
 
 export function Chat() {
-  return <div className={styles["chat"]}>聊天</div>;
+  return (
+    <div className={styles["chat"]}>
+      <p>聊天</p>
+    </div>
+  );
 }
--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -5,14 +5,45 @@ import React from "react";
 import ChatAiIcon from "./static/icons/chatgpt.svg";
 import ChatIcon from "./static/icons/chat.svg";
 import RoleIcon from "./static/icons/role.svg";
+import MaxIcon from "./static/icons/maximize.svg";
+import MinIcon from "./static/icons/minimize.svg";
+import ExitIcon from "./static/icons/exit.svg";
 
 import { useNavigate } from "react-router-dom";
 import { Path } from "@/app/constants";
+import { IconButton } from "./button/button";
+import { useAppConfig } from "@/app/store/config";
 
 export function Sidebar() {
   const navigate = useNavigate();
+  const config = useAppConfig();
+
   return (
     <div className={styles.sidebar}>
+      <div className={styles["action-button"]}>
+        <IconButton
+          icon={<ExitIcon />}
+          backgroundColor={"#ff4e4e"}
+          onClick={() => {
+            alert("尚未实现");
+          }}
+        />
+        <IconButton
+          icon={<MinIcon />}
+          backgroundColor={"#f3c910"}
+          onClick={() => {
+            config.update((config) => (config.tightBorder = false));
+          }}
+        />
+        <IconButton
+          icon={<MaxIcon />}
+          backgroundColor={"#04c204"}
+          onClick={() => {
+            config.update((config) => (config.tightBorder = true));
+          }}
+        />
+      </div>
+
       <div className={styles["sidebar-header"]}>
         <ChatAiIcon />
       </div>
