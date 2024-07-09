b5d7a47023b302f976095fb1a10a244d38becb42
K·Herbert <herbert501@qq.com>
2024-07-09 18:23:21 +0800

    fix: baidu tongji

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,26 +13,8 @@ export default function RootLayout({
   return (
     <html lang="zh-CN">
       <head>
-        {"百度统计"}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              var _hmt = _hmt || [];
-              (function() {
-                var hm = document.createElement("script");
-                hm.src = "https://hm.baidu.com/hm.js?7e88460601019125539907687c3483e4";
-                var s = document.getElementsByTagName("script")[0]; 
-                s.parentNode.insertBefore(hm, s);
-              })();
-            `,
-          }}
-        />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1"
-        />
+        <script src="./static/js/tongji_baidu.js" async></script>
       </head>
-
       <body>{children}</body>
     </html>
   );
